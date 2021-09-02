f313cbe2d6513d64ab3c851e123cc704fc47aaa7
Junho <40736396+MinTpie30@users.noreply.github.com>
2021-09-02 20:04:05 +0900

    Updated front end

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/result')
+def scrappy():
+    return "scrappy"
+
 if __name__ == "__main__":
     app.run(debug=True)
