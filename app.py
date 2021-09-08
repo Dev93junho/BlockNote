859fcbcbe479a27897b00e55b094f64aae3f3911
Junho <40736396+MinTpie30@users.noreply.github.com>
2021-09-08 21:20:55 +0900

    Updated templetes

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ def home():
 
 @app.route('/result')
 def scrappy():
-    pass
-
+    return render_template('scrap-space.html')
+    
+########## after mvp ##############
 @app.route('/loading')
 def loading():
     pass
@@ -23,9 +24,6 @@ def login():
 @app.route('/register')
 def register():
     pass
-
-@app.route
-
 
 if __name__ == "__main__":
     app.run(debug=True)
