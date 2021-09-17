08a97385ae1e645e6c9116163d0240c740229e4e
Junho <jhshin9345@gmail.com>
2021-09-17 14:15:58 +0900

    Updated scrappy original code to app.py

--- app.py
+++ app.py
@@ -19,11 +19,14 @@ def scrappy():
     soup = BeautifulSoup(html_doc, 'html.parser')
 
     data=[]
+
+    
     for no in notebook.tqdm(range(1, 76)):
         url = page_url + str(no)
         f = urllib.request.urlopen(url)
         source = f.read()
-
+        
+        # start the soup 
         soup = BeautifulSoup(source, "html.parser")
         tables=soup.select('table') # fixed html tag
 
@@ -34,8 +37,14 @@ def scrappy():
             add_list = table_df_list[0]
             data.append(add_list)
 
+        # Data framing
         X= np.array(data).reshape(-1, 6)
         df = pd.DataFrame(X)
+
+        # change the order
+        df.columns = ['의안명','발의의원', '상임위', '국회현황', '의결결과','의안번호']
+        final = df[['의안번호', '국회현황','발의의원','의안명','의결결과','상임위']]
+        final_result.to_excel("result.xlsx")
 
 
     return render_template('scrap-space.html')
