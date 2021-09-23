0f8e99beb411924c3a453d6de7ddc06d0d0f9c94
jhshin9345@gmail.com <jhshin9345@gmail.com>
2021-09-24 01:07:15 +0900

    updated initial version

--- windemo_gui.py
+++ windemo_gui.py
@@ -1,12 +1,17 @@
 import sys
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import *
-from core import scrappy
+from bs4 import BeautifulSoup
+import urllib.request
+import numpy as np
+# from tqdm import trange, notebook
+import pandas as pd
+# import requests
 
 
 class Application(QMainWindow):
-    def __init__(self, master=None):
-        super().__init__(master)
+    def __init__(self):
+        super().__init__()
         self.setGeometry(400,250,300,400)
         self.setWindowTitle("data_scrapper_demo")
         self.setWindowIcon(QIcon("icon.png"))
@@ -14,12 +19,44 @@ class Application(QMainWindow):
 
         btn = QPushButton("Download", self)
         btn.move(97, 350)
-        btn.clicked.connect(self.core)
+        btn.clicked.connect(self.scrappy)
     
-    def core(self):
-        scrappy
+    def scrappy(self):
+        page_url = input
+        # soup = BeautifulSoup(html_doc, 'html.parser')
 
-        self.sleep(1)
+
+        page_url = 'https://www.lawmaking.go.kr/opnPtcp/nsmLmSts/out?pageIndex='
+        data=[]
+
+        for no in range(1, 76):
+            url = page_url + str(no)
+            f = urllib.request.urlopen(url)
+            source = f.read()
+            
+            # start the soup 
+            soup = BeautifulSoup(source, "html.parser")
+            tables=soup.select('table') # fixed html tag
+
+            table_html = str(tables)
+            table_df_list = pd.read_html(table_html) # LIST type
+
+            for i in range(1):
+                add_list = table_df_list[0]
+                data.append(add_list)
+
+            # Data framing
+            X= np.array(data).reshape(-1, 6)
+            df = pd.DataFrame(X)
+
+            # change the order
+            df.columns = ['의안명','발의의원', '상임위', '국회현황', '의결결과','의안번호']
+            final = df[['의안번호', '국회현황','발의의원','의안명','의결결과','상임위']]
+            final.to_excel("result.xlsx")
+
+            break
+
+
 
 app = QApplication(sys.argv)
 window = Application()
