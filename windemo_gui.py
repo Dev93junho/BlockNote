d9a5d0f8921a4b157985f302a5adcac18d6d8244
Junho <jhshin9345@gmail.com>
2021-09-23 12:41:27 +0900

    updated demo gui

--- windemo_gui.py
+++ windemo_gui.py
@@ -1,19 +1,36 @@
 import tkinter as tk
 from core import scrappy
 
-scrap = tk.Tk()
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.inner_widgets()
 
-scrap.title("법안자동수집정리머신")
-scrap.geometry("450x600")
-scrap.resizable(True, True)
+    def inner_widgets(self):
+        self.scrap = tk.Button(self)
+        self.scrap["text"] = "Scarp!"
+        self.scrap["command"] = self.scrap_func
+        # self.scrap["overreilf"] = "solid"
+        self.scrap.pack(side="top")
 
-scrappy
+    def scrap_func(self):
+        scrappy
 
-# the button invoke to download excel file
-button = tk.Button(scrap, command=scrappy, overrelief="solid",text="Start", width=15, repeatdelay=1000, repeatinterval=100)
-button.pack()
 
-button.bind('<Return>') 
+root = tk.Tk()
 
-scrap.mainloop()
+# root.title("법안자동수집정리머신")
+# root.geometry("450x600")
+# root.resizable(True, True)
 
+
+app = Application(master=root)
+app.mainloop
+
+# # the button invoke to download excel file
+# button = tk.Button(scrap, command=scrappy, overrelief="solid",text="Start", width=15, repeatdelay=1000, repeatinterval=100)
+# button.pack()
+
+# button.bind('<Return>') 
