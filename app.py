8ac176f3adf6d1966763e55bd801f0ed42e6efd5
jhshin9345@gmail.com <jhshin9345@gmail.com>
2021-10-26 20:35:22 +0900

    updated app.py

--- app.py
+++ app.py
@@ -8,31 +8,31 @@ DEFAULT_HOST = '0.0.0.0'
 
 app = Flask(__name__)
 
+# index page
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('docspace.html')
 
+# display scrappy data to sidebar
 @app.route('/sidebar/<scrap_db>')
 def report(scrap_db):
-    print(request.args.get('url'))
     pass
 
+
+# writing function
+
 # Login module
-# member db
-@app.route('/login')
-def login(methods=['GET','POST']):
-    
+@app.route('/login', methods=['GET','POST'])
+def login():
     pass
 
 @app.route('/register')
 def register():
     pass
 
-# loading page
-@app.route('/loading')
-def loading():
-    return render_template('loading.html')
-
+@app.route('/user/<username>')
+def get_username(username):
+    return 
 
 if __name__ == "__main__":
     app.run(debug=True)
