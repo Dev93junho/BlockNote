785bb93d9f3bedbb500ff3e12faeaf6b12002078
Junho <jhshin9345@gmail.com>
2021-10-28 12:56:06 +0900

    import scrappy to app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request # data를 송수신하는 기능을 가진것은 request
 from werkzeug.utils import secure_filename
 import os
 import scrappy
@@ -23,8 +23,10 @@ def document():
 
 # display scrappy data to sidebar
 @app.route('/sidebar/<scrap_db>')
-def report(scrap_db):
-    pass
+def searched(scrap_db):
+    input = request.args.get('url')
+    scrappy.url_searched(input)
+    pass 
 
 # Login module
 @app.route('/login', methods=['GET','POST'])
