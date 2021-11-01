298bfc845eadd41775ee3829852dea2dd2171b1a
Junho <jhshin9345@gmail.com>
2021-11-01 11:14:10 +0900

    updated CORS

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
 from flask import Flask, render_template, request # data를 송수신하는 기능을 가진것은 request
 from werkzeug.utils import secure_filename
 import os
+from flask_cors import CORS, cross_origin
 import scrappy
 from pymongo import MongoClient
 
 DEFAULT_PORT = 5000
 DEFAULT_HOST = '0.0.0.0'
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='client/build', static_url_path='plz input')
+CORS(app)
+'''
+If you want to change the Environment state to production,
+Go to '.env', rewrite develop to production
+'''
 # import mongoDB
 client = MongoClient('localhost', 27017)
 
-# index page
+# index page    
 @app.route('/', methods=['GET', 'POST'])
+@cross_origin()
 def home():
     return render_template('docspace.html')
+
+def serve():
+    return send_from_directory(app.static_folder, 'docspace.html')
 
 # text editor module
 def editor():
