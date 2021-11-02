268d14c57363b8f4f8491b777cfe342a7cdd22d5
Junho <jhshin9345@gmail.com>
2021-11-02 11:50:59 +0900

    updated serve

--- server/app.py
+++ server/app.py
@@ -8,7 +8,7 @@ from pymongo import MongoClient
 DEFAULT_PORT = 5000
 DEFAULT_HOST = '0.0.0.0'
 
-app = Flask(__name__, static_folder='./client/build', static_url_path='plz input')
+app = Flask(__name__, static_folder='./client/build', static_url_path='')
 CORS(app)
 '''
 If you want to change the Environment state to production,
@@ -18,34 +18,37 @@ Go to '.env', rewrite develop to production
 client = MongoClient('localhost', 27017)
 
 # index page    
+# @app.route('/', methods=['GET', 'POST'])
+# @cross_origin()
+# def home():
+#     pass
+
+# index page    
 @app.route('/', methods=['GET', 'POST'])
 @cross_origin()
-def home():
-    return render_template('docspace.html')
-
 def serve():
-    return send_from_directory(app.static_folder, 'docspace.html')
+    return send_from_directory(app.static_folder, 'index.html')
 
 # display scrappy data to sidebar
-@app.route('/sidebar/<scrap_db>')
-def searched(scrap_db):
-    input = request.args.get('url')
-    scrappy.url_searched(input)
+# @app.route('/sidebar/<scrap_db>')
+# def searched(scrap_db):
+#     input = request.args.get('url')
+#     scrappy.url_searched(input)
     
-    pass
+#     pass
 
 # Login module
-@app.route('/login', methods=['GET','POST'])
-def login():
-    pass
+# @app.route('/login', methods=['GET','POST'])
+# def login():
+#     pass
 
-@app.route('/register')
-def register():
-    pass
+# @app.route('/register')
+# def register():
+#     pass
 
-@app.route('/user/<username>')
-def get_username(username):
-    return 
+# @app.route('/user/<username>')
+# def get_username(username):
+#     return 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
