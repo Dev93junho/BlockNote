237d75b0e4eb16a328ebcda8cca70ee1f20db2af
jhshin9345@gmail.com <jhshin9345@gmail.com>
2021-11-05 12:04:36 +0900

    updated deploy file

--- client/src/Component/Deploy/Deploy.js
+++ client/src/Component/Deploy/Deploy.js
@@ -3,5 +3,5 @@ import TextEditor from '../../TextEditor';
 import Scrapper from '../Scrapper/Scrapper';
 
 export const Deploy = ({prop}) => {
-    return(<div>{prop.tutorial}</div>)
+    return(<TextEditor />)
 }
